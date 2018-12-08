708f6f60cd36f4fefff0fa8d3bdb20e93a4f6fe2
Nikhilesh <nikhileshxiii@gmail.com>
2018-12-08 14:39:43 -0600

    Add Zuul Service to the list

--- ZuulService/src/main/java/com/example/ZuulService/config/SecurityConfig.java
+++ ZuulService/src/main/java/com/example/ZuulService/config/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.example.ZuulService.config;
 
-import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.ZuulService.security.JwtTokenAuthenticationFilter;
+import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
+
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
@@ -11,9 +19,19 @@ public class SecurityConfig extends WebSecurityConfiguration {
     @Autowired
     private JwtConfig jwtConfig;
 
-    protected void configure(HttpStatus http) throws Exception {
-        http
-                .csrf().disable()
-                sessionManagement().sessionCreationPolicy()
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().exceptionHandling().authenticationEntryPoint((req, rsp, e)-> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and().addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                	.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+                	.antMatchers("/gallery"+"/admin/**").hasRole("ADMIN")
+                	.anyRequest().authenticated();
+    }
+    
+    @Bean
+    public JwtConfig jwtConfig() {
+    	return new JwtConfig();
     }
 }
---
+++ ZuulService/src/main/java/com/example/ZuulService/security/JwtTokenAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.example.ZuulService.security;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.ZuulService.config.JwtConfig;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+public class JwtTokenAuthenticationFilter extends OncePerRequestFilter{
+	
+	private final JwtConfig jwtConfig;
+
+	public JwtTokenAuthenticationFilter(JwtConfig jwtConfig) {
+		this.jwtConfig = jwtConfig;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String header = request.getHeader(jwtConfig.getHeader());
+		
+		if(header == null || !header.startsWith(jwtConfig.getPrefix())) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		String token = header.replace(jwtConfig.getPrefix(), "");
+		
+		try {
+			Claims claims = Jwts.parser()
+					.setSigningKey(jwtConfig.getSecret().getBytes())
+					.parseClaimsJws(token)
+					.getBody();
+			String usernamePrincipal = claims.getSubject();
+			
+			if(usernamePrincipal != null) {
+				List<String> authorities = (List<String>) claims.get("authorities");
+				UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+						usernamePrincipal, null, authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		} catch (Exception e) {
+			SecurityContextHolder.clearContext();
+			
+		}
+		filterChain.doFilter(request, response);
+		
+	}
+	
+	
+	
+	
+
+}
